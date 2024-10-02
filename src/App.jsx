7209e3a94b85004fd4f2abfdea5c9699f765dd51
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-10-02 23:20:19 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  <BrowserRouter>
+    <Routes>
+      <Route path='/'>
+
+
+      </Route>
+    </Routes>
+  </BrowserRouter>
+}
+
+export default App
